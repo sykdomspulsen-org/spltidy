d5f9ea61e72a0609d180de263554ebcc37661308
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 12:12:44 +0100

    onAttach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,9 +1,16 @@
 #' @import data.table ggplot2
 #' @importFrom magrittr %>%
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("spltidy", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
+
   packageStartupMessage(paste0(
     "spltidy ",
-    utils::packageDescription("spltidy")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/spltidy"
   ))
